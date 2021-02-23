44ebf5bb3c83d2fd88ff6603de17586d83c0b0b5
SHIVAM VISHWAKARMA <vishwakshivam@gmail.com>
2021-02-23 17:12:11 +0530

    Latest

--- app.js
+++ app.js
@@ -214,13 +214,9 @@ app.post("/login" , function(req , res){
 
 
 
-let port = process.env.PORT;
-if (port == null || port == "")
-
- {
-  port = 3000;
-}
-
+const port = process.env.PORT || 3000;
+ app.get('/' , (req ,res) => res.send(process.env));
+ 
 app.listen(port, function(){
   console.log("Server has started Successfuly");
 });
